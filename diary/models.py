c24a588a6e73caab02f73b298e8b9755c861091c
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-01-12 16:46:57 +0100

    Config write url

--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class People(models.Model):
+    name = models.CharField(max_length=100)
+    picture = models.ImageField(upload_to='picture')
+
+    def __str__(self):
+        return self.name
+    
+
+class Diary(models.Model):
+    title = models.CharField(max_length=100)
+    tags = models.TextField()
+    text = models.TextField()
+    people = models.ManyToManyField(People, null=True, blank=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- diary/views.py
+++ diary/views.py
@@ -3,3 +3,14 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html')
+
+def write(request):
+    if request.method == 'GET':
+        #print(request.method)
+        return render(request, 'write.html')
+    elif request.method == 'POST':
+        titulo = request.POST.get('titulo')
+        tags = request.POST.getlist('tags')
+        pessoas = request.POST.getlist('pessoas')
+        texto = request.POST.get('texto')
+        return HttpResponse(f'{titulo} - {tags} - {pessoas}, - {texto}')
